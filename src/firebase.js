097d5c743f5ef887044071b02cce61838cc9f0c4
Vikash7080 <sengarvikash256@gmail.com>
2025-04-24 03:40:10 +0530

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -10,28 +10,21 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
- const API_KEY = import.meta.env.VITE_API_KEY;
- const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
- const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
- const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
- const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
- const APP_ID = import.meta.env.VITE_APP_ID;
- const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID ;
-
 
 const firebaseConfig = {
- apiKey: API_KEY,
-  authDomain:AUTH_DOMAIN ,
-  projectId:PROJECT_ID,
-  storageBucket:STORAGE_BUCKET,
-  messagingSenderId:MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+ const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
